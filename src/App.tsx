e450560a42bc5a2708dad66b11166cba68100d12
Kolby <kolbyjayce1@gmail.com>
2025-05-09 16:08:38 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { InputLine } from "./components/input-line";
+import { InputHistory } from "./components/input-history";
+
+function App() {
+  return (
+    <>
+      <InputHistory />
+      <InputLine />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/input-line.tsx
@@ -0,0 +1,10 @@
+import { Cursor } from "./cursor";
+
+export function InputLine() {
+  return (
+    <>
+      <div>user@kolby-portfolio:~$</div>
+      <Cursor />
+    </>
+  );
+}
